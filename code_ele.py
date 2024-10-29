e5065375d902e2544490c7ba558e5ec5819f8be5
”Eleanor <eleanor.attali@etu.minesparis.psl.eu>
2024-10-29 15:44:23 +0100

    ajout

---
+++ code_ele.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+
+# %pip install xlrd>=2.0.1
+
+df = pd.read_excel('/Users/eleanorattali/Documents/cours-info/pe-hackathon/données bonheur 2023.xls')
+
+df.head(10)
